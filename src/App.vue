a15bb855754dcb54efb15189ac59fc167a7a1f43
Julio Mafra <julioclmafra@gmail.com>
2023-05-16 16:01:02 -0300

    instalando vue-resource para consumir API

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <div>
+          <h3>{{ foto.titulo }}</h3>
+          <img :src="foto.url" :alt="foto.titulo" />
+        </div>
+      </li>
+    </ul>
 
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+
+  data() {
     return {
-      msg: 'Bem vindo!'
+      titulo: 'Fotografias',
+
+      fotos: [
+        {
+          url: 'https://www.vocenaneve.com.br/app/uploads/2022/07/Denali-1.jpg',
+          titulo: 'Montanha no Gelo'
+        },
+        {
+          url: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/2b/28/01/pedra-da-gavea.jpg?w=500&h=-1&s=1',
+          titulo: 'Rio de Janeiro Montanha Incrivel'
+        }
+
+      ]
     }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+img {
+  width: 350px;
+  height: 250px;
 }
 </style>
